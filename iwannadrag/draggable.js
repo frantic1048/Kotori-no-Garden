d1129573818f8e8f50eb48da7123a189a699c198
frantic1048 <franticblack@163.com>
2014-11-21 16:17:28 +0800

    oh yooo braZ;classie.js by @desandro added

--- iwannadrag/draggable.js
+++ iwannadrag/draggable.js
@@ -1,113 +1,59 @@
-var classModer = new function () {
-  //class modifier
-
-  // xxxActive
-  // toogle/add/remove "avtive" class.
-  // need at least 1 argument.(a DOM element)
-  // single argument (array)          : [DOM element,DOM element...]
-  // multi arguments (DOM elements)   : DOM element,DOM element...
-
-
-  this.toggleActive = function() {
-    for(var i = 0;i < arguments.length;++i){
-      var el = arguments[i];
-      if (!(el.classList.contains('active'))){
-        el.classList.add('active');
-      }
-      else{
-        el.classList.remove('active');
-      }
-    }
-    return 0;
+var iWannaDrag = new function () {
+  //Usege
+  //for draghandler element,add a drag-handle-for propertie name,and set the value as the target element's id;
+  //if leave blank value for drag-handle-for propertie,it means to be drag handler for itself
+  //<... drag-handle-for="id-for-dragging-target" ...></...>
+  var handleFor = 'drag-handle-for',
+      handles = [],
+      draggingElements = [];
+  var startDrag = function (ev) {
+    //push elment will be drag with it data to draggingElements
+    (function (target) {
+      draggingElements.push({'element' : target,
+                             'initElmentPos' : {
+                               'left' : target.getBoundingClientRect().left + (target.style.position == 'absolute' ? window.scrollX : 0 ),
+                               'top' : target.getBoundingClientRect().top + (target.style.position == 'absolute' ? window.scrollY : 0 )
+                              },
+                              'initMousePos' : {
+                                'x' :ev.clientX,
+                                'y' :ev.clientY
+                              }
+                            });
+    })
+    (document.getElementById(ev.target.getAttribute(handleFor) || ev.target));
   };
-
-  this.setActive = function () {
-    for(var i = 0;i < arguments.length;++i){
-      var el = arguments[i];
-      if(!(el.classList.contains('active'))){
-        el.classList.add('active');
-      }
-    }
-    return 0;
+  var durDrag = function (ev) {
+    try {window.getSelection().removeAllRanges();} catch(e) {;}
+    draggingElements.filter(function (oDre) {
+      oDre.element.style.left = (oDre.initElmentPos.left + ev.clientX - oDre.initMousePos.x).toString() + 'px';
+      oDre.element.style.top = (oDre.initElmentPos.top + ev.clientY - oDre.initMousePos.y).toString() + 'px';
+    });
   };
-
-  this.unsetActive = function () {
-    for(var i = 0;i < arguments.length;++i){
-      var el = arguments[i];
-      if(el.classList.contains('active')){
-        el.classList.remove('active');
-      }
-    }
-    return 0;
+  var stopDrag = function () { draggingElements = []; };
+  this.init = function () {
+    handles = Array.prototype.slice.call(document.querySelectorAll('[' + handleFor + ']'));
+    Array.prototype.filter.call(handles,function (el) { el.addEventListener('mousedown',startDrag); });
+    window.addEventListener('mousemove',durDrag);
+    window.addEventListener('mouseup', stopDrag);
   };
 };
-
-var dragBra = new function () {
-  var movingEl = null,   //被拖动的元素
-      triggerEl = null,  //触发拖动的元素
-      lastElPos = {top:null,left:null}, //拖动开始时被拖动的元素位置
-      lastMousePos = {x:null,y:null},   ////拖动开始时鼠标位置
-      isMoving = false;
-
-  var draggingBra = function (ev) {
-    if (isMoving) {
-      movingEl.style.top  = (lastElPos.top  + ev.clientY - lastMousePos.y).toString() + 'px';
-      movingEl.style.left = (lastElPos.left + ev.clientX - lastMousePos.x).toString() + 'px';
-      try {window.getSelection().removeAllRanges();} catch(e) {;}//避免拖动的时候产生选区
-    }
-  };
-
-  var startDragBra = function (ev) {
-    classModer.setActive(movingEl);
-    isMoving = true;
-
-
-    lastMousePos.x = ev.clientX;
-    lastMousePos.y = ev.clientY;
-
-    lastElPos.top = movingEl.getBoundingClientRect().top + (movingEl.style.position == 'absolute' ? window.scrollY : 0 );
-    lastElPos.left = movingEl.getBoundingClientRect().left + (movingEl.style.position == 'absolute' ? window.scrollX : 0 );
-  };
-
-  var stopDragBra = function (ev) {
-    classModer.unsetActive(movingEl);
-    isMoving = false;
-  };
-
-  var getTarget = function () {
-    //get elements
-    movingEl = document.querySelector('#hana-svg');
-    triggerEl = document.querySelector('#hana');
-  };
-
-  var bindEvent = function () {
-    //bind event func
-    triggerEl.addEventListener('mousedown', startDragBra);
-    window.addEventListener('mouseup', stopDragBra);
-    window.addEventListener('mousemove', draggingBra);
-  };
-
-  this.init = function () {
-    getTarget();
-    bindEvent();
-  };
-
+var dradMod = new function () {
   this.setAbsoluteDrag = function (el) {
-    movingEl.style.top  = (movingEl.getBoundingClientRect().top + window.scrollY).toString() + 'px';
-    movingEl.style.left = (movingEl.getBoundingClientRect().left + window.scrollX).toString() + 'px';
-    movingEl.style.position = 'absolute';
+    el.style.top  = (el.getBoundingClientRect().top + window.scrollY).toString() + 'px';
+    el.style.left = (el.getBoundingClientRect().left + window.scrollX).toString() + 'px';
+    el.style.position = 'absolute';
     (function (btns) {
-      classModer.setActive(btns[0]);
-      classModer.unsetActive(btns[1]);
+      classie.add(btns[0], 'active');
+      classie.remove(btns[1], 'active');
     })(document.getElementById('drag-panel').children);
   };
-  this.setFixedDrag = function () {
-    movingEl.style.top  = movingEl.getBoundingClientRect().top.toString() + 'px';
-    movingEl.style.left = movingEl.getBoundingClientRect().left.toString() + 'px';
-    movingEl.style.position = 'fixed';
+  this.setFixedDrag = function (el) {
+    el.style.top  = el.getBoundingClientRect().top.toString() + 'px';
+    el.style.left = el.getBoundingClientRect().left.toString() + 'px';
+    el.style.position = 'fixed';
     (function (btns) {
-      classModer.setActive(btns[1]);
-      classModer.unsetActive(btns[0]);
+      classie.add(btns[1], 'active');
+      classie.remove(btns[0], 'active');
     })(document.getElementById('drag-panel').children);
   };
 };
